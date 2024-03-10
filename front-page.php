77eca22e12ae6fdb9f26d01d7d96e3de02a3aa65
Jovian Almada <jovian-almada@hotmail.com>
2024-03-10 10:59:12 +1000

    updating for work done over the weekend from home

--- front-page.php
+++ front-page.php
@@ -15,9 +15,21 @@
         <div class="full-width-split__inner">
           <h2 class="headline headline--small-plus t-center">Upcoming Events</h2>
 <?php 
+$today = date('Ymd');
 $homepageEvents = new WP_Query(array(
-  'posts_per_page'=>2,
-  'post_type' => 'event'
+  'posts_per_page'=>2,  
+  'post_type' => 'event',
+  'meta_key' => 'event_date',
+  'orderby' => 'meta_value_num',
+  'order' => 'ASC',
+  'meta_query' => array(
+    array(
+      'key' => 'event_date',
+      'compare' => '>=',
+      'value' => $today,
+      'type' => 'numeric'
+    )
+  )
 ));
 
 while($homepageEvents->have_posts()){
@@ -25,8 +37,13 @@ $homepageEvents->the_post();
   ?>
   <div class="event-summary">
             <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-              <span class="event-summary__month"><?php the_time('M'); ?></span>
-              <span class="event-summary__day"><?php the_time('d'); ?></span>
+              <span class="event-summary__month"><?php 
+              $formDate = new DateTime(get_field('event_date'));
+              echo $formDate->format('M');
+              ?></span>
+              <span class="event-summary__day"><?php 
+              echo $formDate->format('d');
+              ?></span>
             </a>
             <div class="event-summary__content">
               <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
--- functions.php
+++ functions.php
@@ -19,6 +19,26 @@ function university_features() {
 
 add_action('after_setup_theme', 'university_features');
 
+// if the query is a not wpadmin based, is an archive of the event and it's a main query and not a custom query
+function university_adjust_queries($query) {
+  $today = date('Ymd');
+  if (!is_admin() AND is_post_type_archive('event') AND $query -> is_main_query()){
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+    array(
+      'key' => 'event_date',
+      'compare' => '>=',
+      'value' => $today,
+      'type' => 'numeric'
+    )
+    ));
+  }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
+
 
 
 
